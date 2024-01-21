8986011c18c9e159f2207a30f6167c5ed148039d
Ruckus <ruckus@fdev.me>
2024-01-21 19:53:55 +0300

    Use polling

--- bot/main.py
+++ bot/main.py
@@ -23,14 +23,16 @@ glv.dp = Dispatcher(storage=glv.storage)
 app = web.Application()
 logging.basicConfig(level=logging.INFO, stream=sys.stdout)
 
+
 async def on_startup(bot: Bot):
-    await bot.set_webhook(f"{glv.config['WEBHOOK_URL']}/webhook")
     asyncio.create_task(register())
+
 
 def setup_routers():
     register_commands(glv.dp)
     register_messages(glv.dp)
     register_callbacks(glv.dp)
+
 
 def setup_middlewares():
     i18n = I18n(path=Path(__file__).parent / 'locales', default_locale='en', domain='bot')
@@ -38,13 +40,11 @@ def setup_middlewares():
     i18n_middleware.setup(glv.dp)
     glv.dp.message.middleware(DBCheck())
 
+
 async def main():
     setup_routers()
     setup_middlewares()
     glv.dp.startup.register(on_startup)
-
-    app.router.add_post("/cryptomus_payment", check_crypto_payment)
-    app.router.add_post("/yookassa_payment", check_yookassa_payment)
     
     webhook_requests_handler = SimpleRequestHandler(
         dispatcher=glv.dp,
@@ -53,7 +53,8 @@ async def main():
     webhook_requests_handler.register(app, path="/webhook")
 
     setup_application(app, glv.dp, bot=glv.bot)
-    await web._run_app(app, host="0.0.0.0", port=glv.config['WEBHOOK_PORT'])
+    await glv.dp.start_polling(glv.bot)
+
 
 if __name__ == "__main__":
     asyncio.run(main())
